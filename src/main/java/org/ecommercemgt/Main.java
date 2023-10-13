0b4b2bc456a00bfc03d0bfecf4f9a3228d274700
ChielokaCode <realmatec01@gmail.com>
2023-10-12 21:41:55 -0700

    updated main file, added print statements

---
+++ src/main/java/org/ecommercemgt/Main.java
@@ -0,0 +1,9 @@
+package org.ecommercemgt;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("E-Commerce Management System");
+        System.out.println("Order products and we deliver to you anywhere in Nigeria");
+    }
+}
