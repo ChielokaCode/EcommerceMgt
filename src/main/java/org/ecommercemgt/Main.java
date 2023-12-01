298378707928a3fda18795133101e3c585040d59
ChielokaCode <realmatec01@gmail.com>
2023-12-01 13:11:11 +0100

    ecommerce
    initial commit

--- src/main/java/org/ecommercemgt/Main.java
+++
@@ -1,9 +0,0 @@
-package org.ecommercemgt;
-
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("E-Commerce Management System");
-        System.out.println("Order products and we deliver to you anywhere in Nigeria");
-    }
-}
